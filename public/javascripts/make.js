14d425b264e49a449ebb76990a665d077e8b6d44
WindSekirun <h.mina.moe@gmail.com>
2017-03-08 01:35:52 +0900

    add listener-callback

---
+++ public/javascripts/make.js
@@ -0,0 +1,12 @@
+$('.btn-make').on('click', function() {
+    $.ajax({
+        url: '/api/makeShort',
+        type: 'POST',
+        dataType: 'JSON',
+        data: {url: $('#url-field').val(), alias: $('#alias-field').val()},
+        success: function(data) {
+            var resultHTML = '<a class="result" href="' + data.shortUrl + '">' + data.shortUrl + '</a>';
+            $('#result').html(resultHTML);
+        }
+    })
+});
