c3581e4e503bcab4a6433a4826405165d081fb86
WindSekirun <h.mina.moe@gmail.com>
2017-03-11 00:25:09 +0900

    implement basic logic of clipboard

--- public/javascripts/make.js
+++ public/javascripts/make.js
@@ -5,12 +5,18 @@ $('.btn-make').on('click', function() {
         dataType: 'JSON',
         data: {url: $('#url-field').val(), alias: $('alias-field').val()},
         success: function(data) {
-            var resultHTML = '<h2>This is for YOU!</h2><h3>Url is <a id="result-url" href="' + data.shortUrl + '">' + data.shortUrl + '</a><h3><button type="button" class="btn btn-primary" id="btn-copy">COPY</button>';
+            var resultHTML = '<h2>This is for YOU!</h2><h3>Url is <a id="result-url" href="' + data.shortUrl + '">' + 
+            data.shortUrl + '</a><h3><button type="button" class="btn btn-primary" data-clipboard-target="#result-url" id="btn-copy">COPY</button>';
             $('#result').html(resultHTML);
         }
     })
 });
 
-$('.btn-copy').on('click', function() {
-    var url = $('result-url').href();
-})
+var clipboard = new Clipboard('.btn-copy');
+clipboard.on('success', function(e) {
+    e.clearSelection();
+});
+
+clipboard.on('error', function(e) {
+
+});
